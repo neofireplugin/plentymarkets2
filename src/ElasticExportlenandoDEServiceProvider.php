078ad17d1490ca1203263924838b5835e3ac6202
lenando <info@lenando.de>
2017-07-20 16:30:32 +0200

    Update ElasticExportlenandoDEServiceProvider.php

--- src/ElasticExportlenandoDEServiceProvider.php
+++ src/ElasticExportlenandoDEServiceProvider.php
@@ -1,38 +1,75 @@
 <?php
 
+
+
 namespace ElasticExportlenandoDE;
 
+
+
 use Plenty\Modules\DataExchange\Services\ExportPresetContainer;
+
 use Plenty\Plugin\DataExchangeServiceProvider;
 
+
+
 /**
+
  * Class ElasticExportlenandoDEServiceProvider
+
  * @package ElasticExportlenandoDE
+
  */
+
 class ElasticExportlenandoDEServiceProvider extends DataExchangeServiceProvider
+
 {
+
     /**
+
      * Abstract function for registering the service provider.
+
      */
+
     public function register()
+
     {
 
+
+
     }
+
+
 
     /**
+
      * Adds the export format to the export container.
+
      *
+
      * @param ExportPresetContainer $container
+
      */
+
     public function exports(ExportPresetContainer $container)
+
     {
+
         $container->add(
+
             'lenandoDE-Plugin',
+
             'ElasticExportlenandoDE\ResultField\lenandoDE',
+
             'ElasticExportlenandoDE\Generator\lenandoDE',
+
             '',
+
             true,
+
             true
+
         );
+
     }
+
 }
